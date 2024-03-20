d42d5f414f91bc0ee29fe69793ea7936910c29a2
Anthony Y <anthony.yang103@gmail.com>
2024-03-20 13:56:30 -0400

    update config_example to match README

--- config_example.py
+++ config_example.py
@@ -1,12 +1,10 @@
-# config.py
-
 # Reddit API Credentials
-CLIENT_ID = 'app_id'
-CLIENT_SECRET = 'your_secret'
-USER_AGENT = 'agent_name'
+CLIENT_ID = 'your_client_id_here'
+CLIENT_SECRET = 'your_client_secret_here'
+USER_AGENT = 'your_user_agent_here'
 
 # Subreddit to scrape
-SUBREDDIT = 'subreddit_name'
+SUBREDDIT = 'name_of_subreddit'
 
 # Number of posts to scrape
 POST_LIMIT = 100
