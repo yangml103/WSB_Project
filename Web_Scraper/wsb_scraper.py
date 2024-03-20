904989c6bcddaa2ad0478854e510ab8da0093459
Anthony Y <anthony.yang103@gmail.com>
2024-03-20 16:57:09 -0400

    Added regex and made new folder to begin sentiment analysis

--- Web_Scraper/wsb_scraper.py
+++ Web_Scraper/wsb_scraper.py
@@ -1,6 +1,8 @@
 import praw
 import config
 import csv
+import re
+
 
 def initialize_reddit():
     """Initialize and return a Reddit instance using PRAW."""
@@ -20,10 +22,20 @@ def fetch_posts(subreddit_name, limit=15):
 
 def fetch_top_comments(post, limit=10):
     """Fetch top comments from a given post."""
-    post.comments.replace_more(limit=0)
+    post.comments.replace_more(limit=0) # Don't include replies to comments
     top_comments = [comment.body for comment in post.comments.list()[:limit]]
-    return top_comments
+    # Define a regex pattern to match the unwanted text
+    pattern = re.compile(
+        r"(\*\*User Report\*\*.*?Account Age.*?years.*?\[.*?\]\(http://discord\.gg/wsbverse\))|"
+        r"(!\[img\]\(emote\|t5_2th52\|\d+\))|"
+        r"(https?://preview\.redd\.it/[\w./?=&-]+)",
+        re.DOTALL
+    )
 
+    # Remove the matched patterns from each comment
+    cleaned_comments = [re.sub(pattern, '', comment) for comment in top_comments]
+    
+    return cleaned_comments 
 def save_posts_to_csv(posts, filename='wsb_posts.csv'):
     """Save posts and their top comments to a CSV file."""
     with open(filename, mode='w', newline='', encoding='utf-8') as file:
