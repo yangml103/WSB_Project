3f71547467adad8f5ca7decaf3b3cd8357b20e53
Anthony Y <anthony.yang103@gmail.com>
2024-03-21 16:50:36 -0400

    renamed variables for better readability

--- Analysis/sentiment_analysis.py
+++ Analysis/sentiment_analysis.py
@@ -5,18 +5,19 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 
 nltk.download('vader_lexicon')
 
-df = pd.read_csv('wsb_posts.csv')
+df_original = pd.read_csv('wsb_posts.csv')
+df_copy = df_original.copy()
 
 sia = SentimentIntensityAnalyzer()
 
-# Assuming the column with comments is named 'comments'
-df['sentiments'] = df['Top Comments'].apply(lambda x: sia.polarity_scores(x))
-df['compound'] = df['sentiments'].apply(lambda x: x['compound'])
+# Assuming the column with comments is named 'Top Comments'
+df_copy['sentiments'] = df_copy['Top Comments'].apply(lambda x: sia.polarity_scores(x))
+df_copy['compound'] = df_copy['sentiments'].apply(lambda x: x['compound'])
 
 # Determine sentiment category based on compound score
-df['sentiment_type'] = df['compound'].apply(lambda x: 'Positive' if x > 0 else ('Negative' if x < 0 else 'Neutral'))
+df_copy['sentiment_type'] = df_copy['compound'].apply(lambda x: 'Positive' if x > 0 else ('Negative' if x < 0 else 'Neutral'))
 
-print(df[['Title', 'sentiment_type']])
+print(df_copy[['Title', 'sentiment_type']])
 
 
 """
